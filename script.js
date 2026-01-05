03805ec3885d070e03f190ea3a6aa5f1a7e7dfef
ajaysingh3686-cyber <ajaysingh3686@gmail.com>
2026-01-05 15:30:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function login() {
     var uid = document.getElementById("uid").value;
     var pwd = document.getElementById("pwd").value;
 
-    if (uid === "DEMO123" && pwd === "Welcome@123") {
+    if (uid === "52222978" && pwd === "M@h@dev_1991") {
         window.location.href = "dashboard.html";
     } else {
         alert("Invalid credentials");
     }
 }
+
